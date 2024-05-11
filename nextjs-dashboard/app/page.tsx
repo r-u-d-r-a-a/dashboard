7741c32ac951e902b7f3da571507d0a837f1162d
rudratarte <rudratarte2604@gmail.com>
2024-05-11 16:23:56 -0400

    commit

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -11,16 +11,12 @@ export default function Page() {
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-5/5 md:px-20">
 
-        <div className='h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20pc] border-b-black border-l-transparent border-r-transparent'/>
+       
 
           <p className={ `${lusitana.className}  text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Welcome to Next.js Dashboard.</strong> Log in to check out the dashboard
           </p>
           <Link
             href="/login"
@@ -29,13 +25,7 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-
-        <Image src="/hero-desktop.png"  width={1000} height={760}  alt="Screenshot of dashboard desktop version"  ></Image>
-        {/* <Image src="/hero-desktop.png" width={1000} height={760}  alt="Screenshot of dashboard mobile version " />   */}
-
-        </div>
+        
       </div>
     </main>
   );
